925ad4afb551c201efabd9cd8f1a3cc682846ea6
sebcourtois <neitsabes974@gmail.com>
2025-04-29 13:43:10 +0200

    part1: 1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const feedbackCount = good + neutral + bad
+    const averageRate = feedbackCount > 0 ? (good - bad) / feedbackCount : 0
+    const positiveRate = feedbackCount > 0 ? good / feedbackCount : 0
+
     return (
         <div>
             <h1>Give Feedback</h1>
@@ -16,6 +20,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {feedbackCount}</p>
+            <p>average {averageRate}</p>
+            <p>positive {positiveRate} %</p>
         </div>
     )
 }
