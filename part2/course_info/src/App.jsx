2dfed4bd80406a2cfebbbe58a7a320a77e0ec62e
sebcourtois <neitsabes974@gmail.com>
2025-05-05 10:44:43 +0200

    part2: 2.2 done

--- part2/course_info/src/App.jsx
+++ part2/course_info/src/App.jsx
@@ -23,18 +23,26 @@ const Content = ({parts}) => {
     )
 }
 
+const Total = ({parts}) => {
+    const totalExercises = parts
+        .map((part) => part.exerciseCount)
+        .reduce((acc, curr) => acc + curr)
+    return <h4>Total of {totalExercises} exercises</h4>
+}
+
 const Course = ({course}) => {
     return (
         <div>
-            <Header title={course.name}/>
+            <Header title={course.title}/>
             <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
         </div>
     )
 }
 
 const App = () => {
     const course = {
-        name: 'Half Stack application development',
+        title: 'Half Stack application development',
         parts: [
             {id: 1, title: 'Fundamentals of React', exerciseCount: 10},
             {id: 2, title: 'Using props to pass data', exerciseCount: 7},
